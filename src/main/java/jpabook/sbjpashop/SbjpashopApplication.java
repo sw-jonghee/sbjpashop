8c2be413c7683679845fe9706829b7c213ef146a
jonghee <sw.jonghee@gmail.com>
2021-09-09 18:35:13 +0900

    project init

---
+++ src/main/java/jpabook/sbjpashop/SbjpashopApplication.java
@@ -0,0 +1,19 @@
+package jpabook.sbjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbjpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println(data);
+
+		SpringApplication.run(SbjpashopApplication.class, args);
+	}
+
+}
